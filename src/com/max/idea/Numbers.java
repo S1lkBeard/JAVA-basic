ce47bc192ab118c91dcc6599cef9a0f700b4b4c1
S1lkBeard <jahwrath@yahoo.com>
2022-04-19 17:03:26 +0300

    Добавлен файл с классом Numbers

---
+++ src/com/max/idea/Numbers.java
@@ -0,0 +1,10 @@
+package com.max.idea;
+
+public class Numbers {
+    public static void main(String[] args) {
+        double a = 64.1;
+        byte b = 127;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
